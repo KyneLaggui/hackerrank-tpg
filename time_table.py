b1450c221781f3b5771d7959f10e9ac61dcff019
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-04-08 12:28:31 +0800

    Fix the negative values of the loop

--- time_table.py
+++ time_table.py
@@ -1,6 +1,10 @@
 def time_table(a, b):
-    for i in range(1, b + 1):
-        print(f"{a} * {i} = {a * i}")
+    if b > 0:
+        for i in range(1, b + 1):
+            print(f"{a} * {i} = {a * i}")
+    else:
+        for i in range(b, 0):
+            print(f"{a} * {i} = {a * i}")
 
 def constraints(a, b):
     if -10 <= a <= 10 and -10 <= b <= 10:
@@ -9,8 +13,8 @@ def constraints(a, b):
         return False
 
 def main():
-    a = int(input("Enter the timetable number: "))
-    b = int(input("Enter the range of timetable: "))
+    a = int(input())
+    b = int(input())
 
     if not constraints(a, b):
         return False
